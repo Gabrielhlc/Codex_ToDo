0ba7788bf1b4441fd7db95a3b4bd382533812783
Gabrielhlc <gabrielhenlc@gmail.com>
2024-04-06 15:31:05 -0300

    server -> front requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+
 require('dotenv').config();
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const port = 3000;
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    description: String,
-    done: Boolean
+    text: String,
+    isChecked: Boolean
 });
 
 const Task = mongoose.model('Task', TaskSchema);
 
 app.get("/", async (req, res) => {
     const tasks = await Task.find();
-    res.send(tasks);
+    return res.json(tasks);
 });
 
 app.delete("/:id", async (req, res) => {
@@ -27,8 +31,8 @@ app.delete("/:id", async (req, res) => {
 
 app.post("/", async (req, res) => {
     const task = new Task({
-        description: req.body.description,
-        done: req.body.done
+        text: req.body.text,
+        isChecked: req.body.isChecked
     });
 
     await task.save();
@@ -37,8 +41,8 @@ app.post("/", async (req, res) => {
 
 app.put("/:id", async (req, res) => {
     const task = await Task.findByIdAndUpdate(req.params.id, {
-        description: req.body.description,
-        done: req.body.done
+        text: req.body.text,
+        isChecked: req.body.isChecked
     })
 
     res.send(task);
@@ -48,4 +52,3 @@ app.listen(port, () => {
     mongoose.connect(`mongodb+srv://${process.env.MONGODB_PASSWORD}/?retryWrites=true&w=majority&appName=codex-todo`);
     console.log("App running on port " + port);
 });
-
